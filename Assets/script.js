034f8a7aa4cc5fffdca1dc3b83100aeee11a60a1
Marni Luka <marnixluka@gmail.com>
2023-04-17 15:10:55 -0400

    put the spoonacular api key into JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,15 @@
 console.log ("text");
+
+// Spoonacular API: apiKey=1b75e20facc84590ba826c63864d26c8
+// Use this key when using the fetch function
+
+var recipes = 'https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=1b75e20facc84590ba826c63864d26c8'
+
+fetch (recipes)
+.then (function (response) {
+    return response.json();
+})
+.then (function (data) {
+    console.log(data);
+    
+})
